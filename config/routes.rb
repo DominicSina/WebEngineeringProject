385116f83f8b02ab6cc6c4a6f2ac96dd49a587d4
DominicSina <dominic.sina@gmx.ch>
2016-12-02 01:30:24 +0100

    Installed Sorcery for user auth, created user table, login/logut and made authenticated routes for user paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+
+  #are handled by sorcery, so these routes should not be accessible
+  #get 'user_sessions/new'
+  #get 'user_sessions/create'
+  #get 'user_sessions/destroy'
+
+  #get route for welcome_page
   get 'welcome_page/welcome'
-  root 'welcome_page#welcome'
+  #makes welcome page the root page
+  #root 'welcome_page#welcome'
+
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
