f2ebe2d78a50ab3e0def9f8cde526ac15fa5998d
DominicSina <dominic.sina@gmx.ch>
2016-12-17 00:42:26 +0100

    new bids now have to be higher than the current largest bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,5 +1,14 @@
 class Bid < ApplicationRecord
   belongs_to :user
   belongs_to :auction
-  has_many :bids
+
+  validates :auction_id, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true }
+  validate :bigger_than_highest_bid_check
+
+  def bigger_than_highest_bid_check
+    if self.amount < self.auction.highestBid.amount
+      errors.add(:amount, "has to be higher than current highest bid#{self.auction.highestBid.amount}.")
+    end
+  end
 end
