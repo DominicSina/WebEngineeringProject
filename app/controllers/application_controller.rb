1da5ef6c9524336bb1b4701385736d0b0e6c2a64
DominicSina <dominic.sina@gmx.ch>
2016-12-18 04:00:38 +0100

    rescued from Pundid:NotAuthorizedError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   #protect_from_forgery #pundit setup requires this but the protect_from_forgery above should cover this
 
   private
+  def user_not_authorized
+    #flash.now[:alert] = "Not autorized"
+    redirect_to auctions_path, alert: "Not autorized"
+  end
+
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
